c69d4937dde36c9492d891f359010a7d538bf079
Nicky <nickysingh4242@gmail.com>
2022-08-27 19:10:41 +0530

    first commit

---
+++ src/components/Array.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+const no_Of_array_bars = 310;
+
+function randomIntFromInterval(min,max){
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+const Array = () => {
+  return (
+    <>
+    <h1>hello</h1>
+    </>
+  )
+}
+
+export default Array
